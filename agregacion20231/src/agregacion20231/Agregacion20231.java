dd3e35019da54160c600ce2b6c11dec4fb25d5ac
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-02-13 10:01:24 -0600

    commit 1302202312813

--- agregacion20231/src/agregacion20231/Agregacion20231.java
+++ agregacion20231/src/agregacion20231/Agregacion20231.java
@@ -22,7 +22,20 @@ public class Agregacion20231 {
         //Operadores de servicios telefonicos
         Operador tigo = new Operador("CELTEL","Honduras");
         Operador claro = new Operador("Claro de Honduras","Honduras");
-        
+        //Baterias para smartphone
+        Bateria batSony = new Bateria(sony, 4000);
+        Bateria batSamsung = new Bateria(samsung, 4500);
+        Bateria batApple = new Bateria(apple, 6000);
+        //Chips
+        Chip sim1 = new Chip(tigo, 99995599);
+        Chip sim2 = new Chip(claro, 33331133);
+        Chip sim3 = new Chip(tigo, 98981298);
+        //Smartphones
+        Smartphone s22 = new Smartphone(samsung, "S22 lite");
+        s22.almacenamiento = 128;   s22.ram = 6;
+        s22.bateria = batSamsung;   s22.chip1 = sim3;
+        s22.chip2 = sim1;       
+        s22.imprimir();
     }
     
 }
--- agregacion20231/src/agregacion20231/Smartphone.java
+++ agregacion20231/src/agregacion20231/Smartphone.java
@@ -29,6 +29,38 @@ public class Smartphone {
      */
     public void imprimir(){
         System.out.println("*********** SMARTPHONE ***********");
-        System.out.println("Fabricante: "+marca);
+        System.out.println("Fabricante: "+this.marca.nombre);
+        System.out.println("Modelo: "+this.modelo);
+        System.out.println("Almacenamiento: "+this.almacenamiento+"GB");
+        System.out.println("RAM: "+this.ram+"GB");
+        //comprobar si tiene bateria:
+        if( this.bateria == null )
+            System.out.println("Bateria: No tiene");
+        else
+        {
+            System.out.println("Bateria *****");
+            System.out.println("\tMarca: "+this.bateria.marca.nombre);
+            System.out.println("\tMiliAmperios: "+this.bateria.miliAmperios);
+        }
+        //comprobar si tiene chip1:
+        if( this.chip1 == null )
+            System.out.println("Chip 1: no tiene");
+        else
+        {
+            System.out.println("Chip 1 *******");
+            System.out.println("\tProveedor de servicios: "+this.chip1.proveedor.nombre);
+            System.out.println("\tPais: "+this.chip1.proveedor.pais);
+            System.out.println("\tNumero de telefono: "+this.chip1.numeroTelefonico);
+        }
+        //comprobar si tiene chip2:
+        if( this.chip2 == null )
+            System.out.println("Chip 2: no tiene");
+        else
+        {
+            System.out.println("Chip 2 *******");
+            System.out.println("\tProveedor de servicios: "+this.chip2.proveedor.nombre);
+            System.out.println("\tPais: "+this.chip2.proveedor.pais);
+            System.out.println("\tNumero de telefono: "+this.chip2.numeroTelefonico);
+        }
     }
 }
