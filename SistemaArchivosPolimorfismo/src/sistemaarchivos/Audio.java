2905d6bffc2a64535fa098236b3c4fd32968da3c
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-03-10 09:49:58 -0600

    commit 1003202319104

--- SistemaArchivosPolimorfismo/src/sistemaarchivos/Audio.java
+++ SistemaArchivosPolimorfismo/src/sistemaarchivos/Audio.java
@@ -22,6 +22,19 @@ public class Audio extends Media {
     //metodos
     @Override
     public String toString(){
+        //obtener el stoString de la super clase como
+        //punto de partida
+        //Media hereda toString() de File
+        //Audio sobreescribe toString() de la clase Media
         String str = super.toString();
+        //anexar al String los datos de Audio y Media
+        str += "\nAuthor: "+this.author.nombre;
+        str += "\nDuration: "+this.duration+" seconds.";
+        str += "\nArtist: "+this.artist;
+        str += "\nAlbum: "+this.album;
+        str += "\nYear: "+this.year;
+        str += "\nTrack Number: "+this.track;
+        
+        return str;
     }
 }
--- SistemaArchivosPolimorfismo/src/sistemaarchivos/Video.java
+++ SistemaArchivosPolimorfismo/src/sistemaarchivos/Video.java
@@ -18,4 +18,34 @@ public class Video extends Media {
         this.audio = null;
         this.subtitles = null;
     }
+    //metodos
+    @Override
+    public String toString(){
+        //obtener el stoString de la super clase como
+        //punto de partida
+        //Media hereda toString() de File
+        //Video sobreescribe toString() de la clase Media
+        String str = super.toString();
+        //anexar al String los datos de Video y Media
+        str += "\nAuthor: "+this.author.nombre;
+        str += "\nDuration: "+this.duration+" seconds.";
+        str += "\nCodec: "+this.codec;
+        str += "\nNumber of Chapters: "+this.chapters;
+        //canales de audio disponibles
+        str += "\nAudio channels:";
+        for( int i = 0; i < this.audio.length; i++ ){
+            //validacion de null
+            if( this.audio[i] != null )
+                str += "\n\t"+this.audio[i];
+        }
+        //subtitulos disponibles
+        str += "\nAvailable subtitles:";
+        for( int i = 0; i < this.subtitles.size(); i++ ){
+            //validacion de null
+            if( this.subtitles.get(i) != null )
+                str += "\n\t"+this.subtitles.get(i).language+" ("+this.subtitles.get(i).author.nombre+")";
+        }
+        
+        return str;
+    }
 }
